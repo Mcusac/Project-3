01d1afbde59b6af5ff7085a7b84d70876c8efd37
Mcusac <matthew.d.cusack@gmail.com>
2022-07-05 18:27:08 -0500

    tues1

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:{port}/mars_app")
+mongo = PyMongo(app, uri="mongodb://localhost:{port}/baseball_db")
 
 
 # Route to render index.html template using data from Mongo
-# @app.route("/")
-# def home():
+@app.route("/")
+def home():
 
-#     # Find one record of data from the mongo database
-#     mars = mongo.db.collection.find_one()
+    # Find one record of data from the mongo database
+    baseball = mongo.db.collection.find_one()
 
-#     # Return template and data
-#     return render_template("index.html", mars=mars)
+    # Return template and data
+    return render_template("index.html", baseball=baseball)
 
 if __name__ == "__main__":
     app.run(debug=True)
