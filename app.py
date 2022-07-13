4b8e5108f263d189b7265d6d190e7a28d23aad1b
Mcusac <matthew.d.cusack@gmail.com>
2022-07-12 19:22:48 -0500

    tues1

--- app.py
+++ app.py
@@ -41,10 +41,6 @@ def home1():
 def home2():
     return render_template("index.html")
 
-# @app.route("/api/v1.0")
-# def api():
-#     return f'please work'
-
 @app.route("/about")
 def about():
     print("Server received request for 'About' page...")
@@ -56,22 +52,9 @@ def pitching_api():
     session = Session(engine)
     pitching_result = session.query(Pitching.playerID, Pitching.yearID, Pitching.HR, Pitching.SO, Pitching.BB, Pitching.ERA, Pitching.first, Pitching.last).all()
     # looks at at each tuple and turns into a list 
-    pitching_result = [list(r) for r in pitching_result]
+    pitching_result = [list(p) for p in pitching_result]
     session.close()
     return jsonify(pitching_result)
-
-# @app.route("/api/v1.0/pitching/<playerID>")
-# def pitching_api_player(playerID):
-#     session = Session(engine)
-#     pitching_result = session.query(Pitching.playerID, Pitching.yearID, Pitching.HR, Pitching.SO, Pitching.BB, Pitching.ERA, Pitching.first, Pitching.last).all()
-#     # looks at at each tuple and turns into a list 
-#     pitching_result = [list(r) for r in pitching_result]
-#     session.close()
-#     pitching_final = []
-#     for pitcher in pitching_result:
-#         if pitcher[0] == playerID:
-#             pitching_final.append(pitcher)
-#     return jsonify(pitching_final)
 
 @app.route("/api/v1.0/batting")
 def batting_api():
@@ -79,31 +62,17 @@ def batting_api():
     # batting_result = session.query(Batting.AVG).all()
     batting_result = session.query(Batting.playerID, Batting.yearID, Batting.AVG, Batting.HR, Batting.single_per, Batting.double_per, Batting.triple_per, Batting.HRper, Batting.SO).all()
     # looks at at each tuple and turns into a list 
-    batting_result = [list(r) for r in batting_result]
+    batting_result = [list(b) for b in batting_result]
     session.close()
     return jsonify(batting_result)
-
-@app.route("/api/v1.0/batting/<playerID>")
-def batting_api_player(playerID):
-    session = Session(engine)
-    # batting_result = session.query(Batting.AVG).all()
-    batting_result = session.query(Batting.playerID, Batting.yearID, Batting.AVG, Batting.HR, Batting.single_per, Batting.double_per, Batting.triple_per, Batting.HRper, Batting.SO).all()
-    # looks at at each tuple and turns into a list 
-    batting_result = [list(r) for r in batting_result]
-    session.close()
-    batting_final = []
-    for batter in batting_result:
-        if batter[0] == playerID:
-            batting_final.append(batter)
-    return jsonify(batting_final)
 
 @app.route("/batting")
 def batting():
     return render_template("batting.html")
 
-@app.route("/form_batting")
+@app.route("/batting_data")
 def form_batting():
-    return render_template("form_batting.html")
+    return render_template("batting_data.html")
 
 
 @app.route("/pitching")
@@ -111,42 +80,42 @@ def pitching():
     return render_template("pitching.html")
 
 
-@app.route("/form_pitching")
+@app.route("/pitching_data")
 def form_pitching():
-    return render_template("form_pitching.html")
+    return render_template("pitching_data.html")
 
 #--------------------------------------------------------------------------------------------------------
-# This was the route that Bill helped us out with to get the batting form to work.
-@app.route("/send_batting/<yearID>/<playerID>", methods=["GET", "POST"])
-def send_batting(yearID, playerID):
+# # This was the route that Bill helped us out with to get the batting form to work.
+# @app.route("/send_batting/<yearID>/<playerID>", methods=["GET", "POST"])
+# def send_batting(yearID, playerID):
 
-    session = Session(engine)
+#     session = Session(engine)
 
-    print(yearID, playerID)
-    batting_result = session.query(Batting.playerID, Batting.yearID, Batting.AVG, Batting.HR, Batting.single_per, Batting.double_per, Batting.triple_per, Batting.HRper, Batting.SO).filter(Batting.playerID == playerID).filter(Batting.yearID == yearID).all()    
-    print(batting_result)
-    batting_result = [list(b) for b in batting_result]
-    session.close()
+#     print(yearID, playerID)
+#     batting_result = session.query(Batting.playerID, Batting.yearID, Batting.AVG, Batting.HR, Batting.single_per, Batting.double_per, Batting.triple_per, Batting.HRper, Batting.SO).filter(Batting.playerID == playerID).filter(Batting.yearID == yearID).all()    
+#     print(batting_result)
+#     batting_result = [list(b) for b in batting_result]
+#     session.close()
 
-    session.close()
+#     session.close()
 
-    return jsonify(batting_result)
+#     return jsonify(batting_result)
 
-#--------------------------------------------------------------------------------------------------------
-# This was the route that Bill helped us out with to get the pitching form to work.
+# #--------------------------------------------------------------------------------------------------------
+# # This was the route that Bill helped us out with to get the pitching form to work.
 
-@app.route("/send_pitching/<yearID>/<playerID>", methods=["GET", "POST"])
-def send_pitching(yearID, playerID):
+# @app.route("/send_pitching/<yearID>/<playerID>", methods=["GET", "POST"])
+# def send_pitching(yearID, playerID):
 
-    session = Session(engine)
+#     session = Session(engine)
     
-    print(yearID, playerID)    
-    pitching_result = session.query(Pitching.playerID, Pitching.yearID, Pitching.HR, Pitching.SO, Pitching.BB, Pitching.ERA, Pitching.first, Pitching.last).filter(Pitching.playerID == playerID).filter(Pitching.yearID == yearID).all()
-    print(pitching_result)
-    pitching_result = [list(p) for p in pitching_result]
-    session.close()
+#     print(yearID, playerID)    
+#     pitching_result = session.query(Pitching.playerID, Pitching.yearID, Pitching.HR, Pitching.SO, Pitching.BB, Pitching.ERA, Pitching.first, Pitching.last).filter(Pitching.playerID == playerID).filter(Pitching.yearID == yearID).all()
+#     print(pitching_result)
+#     pitching_result = [list(p) for p in pitching_result]
+#     session.close()
 
-    return jsonify(pitching_result)
+#     return jsonify(pitching_result)
 #--------------------------------------------------------------------------------------------------------
 # Matt trying to put them on the same page
 
@@ -163,7 +132,7 @@ def pitching_api_year_player(yearID, playerID):
 
     return jsonify(pitching_result)
 
-@app.route("/send_batting/<yearID>/<playerID>", methods=["GET", "POST"])
+@app.route("/batting/<yearID>/<playerID>", methods=["GET", "POST"])
 def batting_api_year_player(yearID, playerID):
 
     session = Session(engine)
--- static/js/app.js
+++ static/js/app.js
@@ -1,68 +1,25 @@
-// Not sure if we need this function anymore, but I left it just in case. 
-// d3.json("api/v1.0/pitching").then(function(data) {
-//     console.log(data);
-
-//     // DROPDOWN
-//         // Use D3 to select the dropdown menu
-//         let dropdownMenu = d3.select("#selDataset_pitching");
-//         // Assign the value of the dropdown menu option to a variable
-//          data.forEach((id) => {
-//              // console.log(id)
-//              dropdownMenu.append("option").text(id[0]).property("value", id[0]);
-            
-//         })
-//         // loadcharts(data.names[0])  
-// });
-
-// Not sure if we need this function anymore, but I left it just in case. 
-// d3.json("api/v1.0/batting").then(function(data) {
-//     console.log(data);
-//     // DROPDOWN
-//         // Use D3 to select the dropdown menu
-//         let dropdownMenu = d3.select("#selDataset");
-//         // Assign the value of the dropdown menu option to a variable
-//          data.forEach((id) => {
-//              // console.log(id)
-//              dropdownMenu.append("option").text(id[0]).property("value", id[0]); 
-//         })
-//         // loadcharts(data.names[0])  
-// });
-
-
-// Not sure if we need this function anymore, but I left it just in case. 
-// function loadcharts(selected){
-//     d3.json("samples.json").then(function(data){
-//         console.log(selected)
+// //-----------------------------------------------------------------------------------
+// // We created the function "batting" below which is being called 
+// // in the form_batting.html file on line 39 as the action =
+// function batting(yearID, playerID) {
+//     console.log(yearID, playerID)
+//     d3.json("/send_batting/"+yearID+"/"+playerID).then(function(data){
+//         console.log(data)
+//         //This is where the plotly charts will go for the Batters
 //     })
 // };
+// //-----------------------------------------------------------------------------------
 
-// Not sure if we need this function anymore, but I left it just in case. 
-// function optionChanged(this_id){
-//     loadcharts(this_id)
+// //-----------------------------------------------------------------------------------
+// // We created the function "pitching" below which is being called 
+// // in the form_pitching.html file on line 39 as the action =
+// function pitching(yearID, playerID) {
+//     console.log(yearID, playerID)
+//     d3.json("/send_pitching/"+yearID+"/"+playerID).then(function(data){
+//         console.log(data)
+//         //This is where the plotly charts will go for the Pitchers
+//     })
 // };
-
-//-----------------------------------------------------------------------------------
-// We created the function "batting" below which is being called 
-// in the form_batting.html file on line 39 as the action =
-function batting(yearID, playerID) {
-    console.log(yearID, playerID)
-    d3.json("/send_batting/"+yearID+"/"+playerID).then(function(data){
-        console.log(data)
-        //This is where the plotly charts will go for the Batters
-    })
-};
-//-----------------------------------------------------------------------------------
-
-//-----------------------------------------------------------------------------------
-// We created the function "pitching" below which is being called 
-// in the form_pitching.html file on line 39 as the action =
-function pitching(yearID, playerID) {
-    console.log(yearID, playerID)
-    d3.json("/send_pitching/"+yearID+"/"+playerID).then(function(data){
-        console.log(data)
-        //This is where the plotly charts will go for the Pitchers
-    })
-};
 //-----------------------------------------------------------------------------------
 // Matt's try at simplification
 function pitching_api_year_player(yearID, playerID) {
@@ -75,7 +32,7 @@ function pitching_api_year_player(yearID, playerID) {
 
 function batting_api_year_player(yearID, playerID) {
     console.log(yearID, playerID)
-    d3.json("/pitching/"+yearID+"/"+playerID).then(function(data){
+    d3.json("/batting/"+yearID+"/"+playerID).then(function(data){
         console.log(data)
         //This is where the plotly charts will go for the Pitchers
     })
