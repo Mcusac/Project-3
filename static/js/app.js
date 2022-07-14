3ab64935e9b19f5146559093c6f897db21b9a9db
Mcusac <matthew.d.cusack@gmail.com>
2022-07-14 17:01:18 -0500

    thurspre

--- static/js/app.js
+++ static/js/app.js
@@ -216,53 +216,27 @@ function batting_api_year_player(yearID, playerID){
       Plotly.newPlot('bgraph3', data1, layout);
     
     // Pie Chart
-    // labels = [
-    //   'Singles',
-    //   'Doubles',
-    //   'Triples',
-    //   'Home Runs'
-    // ]
-    // trace1 = {
-
-    //   values: data['Year'],
-    //   labels: labels,
-    //   type: 'pie'
-    // }
-    //   bat_data = [trace1]
-    //   layout = {
-    //     title: 'Base Hit Breakdown' 
-    //   }
-    //   Plotly.newPlot('bgraph4', bat_data, layout);
+    console.log(data)
+    labels = [
+      'Singles',
+      'Doubles',
+      'Triples',
+      'Home Runs'
+    ]
+    trace1 = {
+      // I think error is here
+      values: data['Batting'],
+      labels: labels,
+      type: 'pie'
+    }
+      bat_data = [trace1]
+      layout = {
+        title: 'Base Hit Breakdown',
+        height: 400,
+        width: 400,
+        showlegend: true
+      }
+      Plotly.newPlot('bgraph4', bat_data, layout);
   })
 }
 //-----------------------------------------------------------------------------------
-// bar chart
-//Plot 1 - AVG
-//xvalue = [First Year -> Last]
-//yvalue = Array values by year
-
-
-// let xValue = ['Product A', 'Product B', 'Product C'];
-// let yValue = [20, 14, 23];
-// let trace1 = {
-//   x: xValue,
-//   y: yValue,
-//   type: 'bar',
-//   text: yValue.map(String),
-//   textposition: 'auto',
-//   hoverinfo: 'none',
-//   marker: {
-//     color: 'rgb(158,202,225)',
-//     opacity: 0.6,
-//     line: {
-//       color: 'rgb(8,48,107)',
-//       width: 1.5
-//     }
-//   }
-// };
-// let data = [trace1];
-// let layout = {
-//   title: 'January 2013 Sales Report',
-//   barmode: 'stack'
-// };
-// Plotly.newPlot('myDiv', data, layout);
